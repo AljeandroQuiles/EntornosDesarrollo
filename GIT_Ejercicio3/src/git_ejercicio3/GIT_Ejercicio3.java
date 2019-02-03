ca5d0c9e29a9793bbf006b6fdf59593c685e9a61
ALEJANDRO <ALEJANDRO@DESKTOP-G2FMFRF.home>
2019-02-03 15:45:11 +0100

    Primeros cambios del archivo

---
+++ GIT_Ejercicio3/src/git_ejercicio3/GIT_Ejercicio3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_ejercicio3;
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class GIT_Ejercicio3 {
+//HEMOS MODIFICADO EL DOCUMENTO CON ESTE COMENTARIO
+    
+    public int numero(){
+    return 1;}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
